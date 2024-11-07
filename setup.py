2085ffa909f2ffba152ff1379e61c144b8a4f8c7
Abdul Ghaffar Ansari <88545317+abdulghaffaransari@users.noreply.github.com>
2024-11-07 21:50:26 +0100

    Requirements.txt and setup.py updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Abdul Ghaffar Ansari",
+    author_email="abdulghaffaransari9@gmail.com",
+    url="https://github.com/abdulghaffaransari/US-Visa-Prediction-System",
+    packages=find_packages()
+)
