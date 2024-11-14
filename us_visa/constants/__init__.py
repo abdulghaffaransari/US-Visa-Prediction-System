df56fb35e2bd062a2b884611de6b142ce2cbd971
Abdul Ghaffar Ansari <88545317+abdulghaffaransari@users.noreply.github.com>
2024-11-14 19:55:29 +0100

    Constants updated

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -17,6 +17,20 @@ TEST_FILE_NAME: str = "test.csv"
 FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME = "model.pkl"
 
+
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
+
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
